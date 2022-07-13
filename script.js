3c64188f7b2288ee63f78a02b0ccadd6da15b3d6
john baby <oliver2002john@gmail.com>
2022-07-13 14:01:23 -0400

    Completes the assignment

--- script.js
+++ script.js
@@ -1,8 +1,28 @@
 const  $gallery = document.getElementById('gallery')
+const  $modal = document.querySelector('.modal')
+const  $modalimage = document.querySelector('.modal-image')
+const  $button = document.querySelector('.button')
+
 
 const  gallery =[]
 
-for (let  i = 0; i <=12; i++) {
-    gallery.push('<img src="images/${i}.jpg' alt = "Pexel image">)
+for (let  i = 1; i <=12; i++) {
+    gallery.push(`<img src="images/${i}.jpg" alt="Pexel image" class="each-img">`)
 }
 $gallery.innerHTML = gallery.join('')
+
+function interactiveGallery(e) {
+
+    const $selectedImages = e.target.closest('.each-img')
+    console.log($selectedImages)
+    $modalimage.src = $selectedImages.src
+    $modal.classList.add('show')
+
+    function returnButton() {
+        $modal.classList.remove('show')
+    }
+    $button.addEventListener('click', returnButton)
+
+}
+$gallery.addEventListener('click', interactiveGallery)
+
